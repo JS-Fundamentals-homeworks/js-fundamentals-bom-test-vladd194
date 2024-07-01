9858d8fcbae330d7f2427be26420bbd511747fcb
vladd194 <vladtsvihun@gmail.com>
2024-07-01 20:07:37 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,16 +1,10 @@
-// Для заданої HTML-сторінки - index.html коментар task1
-// увійдіть у файл task1.js
-// напишіть функцію changeCSS(), яка спрацьовуватиме по кліку на кнопку
-//  і змінюватиме стиль вмісту тега <p>: колір шрифту – оранжевий, розмір шрифту 20px, шрифт сімейства "Comic Sans MS".
-// Запустіть програму за допомогою Live Server
-// перевірте виконання за допомогою команди node tests/task1.test.js
+const paragraph = document.getElementById('p2');
 
-const paragraph = document.getElementById('p');
-                                          
-function changeCSS(){
-  paragraph.style.color = 'orange';
-  paragraph.style.fontSize = '20px';
-  paragraph.style.fontFamily = 'Comic Sans MS';
+function changeCSS() {
+    paragraph.style.color = 'orange';
+    paragraph.style.fontSize = '20px';
+    paragraph.style.fontFamily = 'Comic Sans MS';
 }
-const button = document.getElementById('change-button');
+
+const button = document.getElementById('your-button-id'); /
 button.addEventListener('click', changeCSS);
