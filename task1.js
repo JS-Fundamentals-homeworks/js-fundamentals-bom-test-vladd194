1ca23ec9e2c2068f8ee60084d626281bd8020bbc
vladd194 <vladtsvihun@gmail.com>
2024-07-01 20:03:09 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -5,3 +5,12 @@
 // Запустіть програму за допомогою Live Server
 // перевірте виконання за допомогою команди node tests/task1.test.js
 
+const paragraph = document.getElementById('p2');
+                                          
+function changeCSS(){
+  paragraph.style.color = 'orange';
+  paragraph.style.fontSize = '20px';
+  paragraph.style.fontFamily = 'Comic Sans MS';
+}
+const button = document.getElementById('your-button-id');
+button.addEventListener('click', changeCSS);
